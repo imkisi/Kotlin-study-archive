244fa497c9343460b61559e00ce9399e3ddee659
Bagas "KiSI <80464785+imkisi@users.noreply.github.com>
2025-05-09 19:07:30 +0700

    Update : project

--- Unreleased/app/src/main/java/com/example/unreleased/NameCard.kt
+++ Unreleased/app/src/main/java/com/example/unreleased/NameCard.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.unreleased.ui.theme.UnreleasedTheme
 
 class NameCard : ComponentActivity() {
@@ -23,7 +38,7 @@ class NameCard : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ProfileCard()
                 }
             }
         }
@@ -31,17 +46,89 @@ class NameCard : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ProfileCard(
+    modifier: Modifier = Modifier
+) {
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(),
+            modifier = modifier
+                .size(335.dp, 510.dp)
+                .padding(18.dp)
+        ) {
+            Column(
+                modifier = modifier
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.profile_name),
+                    fontSize = 48.sp,
+                    lineHeight = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = stringResource(R.string.profile_title),
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 8.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_phone_24),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(end = 8.dp)
+                            .size(18.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.profile_phone),
+                        fontSize = 14.sp
+                    )
+                }
+                Row(
+                    modifier = Modifier,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_email_24),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(end = 8.dp)
+                            .size(18.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.profile_mail),
+                        fontSize = 14.sp
+                    )
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "Profile"
+)
 @Composable
 fun GreetingPreview4() {
     UnreleasedTheme {
-        Greeting("Android")
+        ProfileCard()
     }
 }
